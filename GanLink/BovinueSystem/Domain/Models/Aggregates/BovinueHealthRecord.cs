fa6b2af9d078475931285b461de1d3fbd89897b0
u20211c245 (Huaman Hinostroza, Milenio) <u20211c245@upc.edu.pe>
2025-09-29 15:38:41 -0500

    feat: create class heath and record
    # Conflicts:
    #	GanLink/BovinueSystem/Domain/Models/Aggregates~HEAD

---
+++ GanLink/BovinueSystem/Domain/Models/Aggregates/BovinueHealthRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace GanLink.BovinueSystem.Domain.Models.Aggregates;
+
+public partial class BovineHealthRecord
+{
+    public int Id { get; private set; }
+    public int BovineCHRId { get; private set; } // FK a catálogo/registro de tipo CHR
+    public int BovineId { get; private set; }    // FK al bovino
+    public DateTime StartDate { get; private set; }
+    public DateTime EndDate { get; private set; }
+}
