504df85e1f6a37d53c3acd37f5ca0f0456a72984
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-10-03 13:57:35 -0500

    feat(extensions): add repositorÂries and services without implementation,

---
+++ GanLink/IAM/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using GanLink.IAM.Domain.Models.Aggregates;
+using GanLink.Shared.Domain.Repositories;
+
+namespace GanLink.IAM.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User?> GetUserByIdAsync(int id);
+    Task<User?> FindUserByEmail(string email);
+    Task<User?> FindUserByUsername(string username);
+}
