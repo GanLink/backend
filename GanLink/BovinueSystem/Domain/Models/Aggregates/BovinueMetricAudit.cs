3062bcfd27531f25cdee64f4fb48c67e44058a89
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-10-02 20:26:01 -0500

    feat(extensions): create class for bovinue metric, audit class and commands records.

---
+++ GanLink/BovinueSystem/Domain/Models/Aggregates/BovinueMetricAudit.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace GanLink.BovinueSystem.Domain.Models.Aggregates;
+
+public partial class BovinueMetric : IEntityWithCreatedUpdatedDate
+{
+    /// <summary>Fecha/hora de creación (mapeada a 'CreatedAt').</summary>
+    [Column("CreatedAt")]
+    public DateTimeOffset? CreatedDate { get; set; }
+
+    /// <summary>Fecha/hora de última actualización (mapeada a 'UpdatedAt').</summary>
+    [Column("UpdatedAt")]
+    public DateTimeOffset? UpdatedDate { get; set; }
+}
---
+++ GanLink/BovinueSystem/Domain/Models/Commands/CreateBovinueMetricCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace GanLink.BovinueSystem.Domain.Models.Commands;
+
+public record CreateBovinueMetricCommand(
+    long bovinueId,
+    long bovinueMPId,
+    DateTime date,
+    double quantity
+);
