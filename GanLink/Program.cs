56bf0bee4d25df9bb7b861a1a52735365743ba1c
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-10-06 10:50:09 -0500

    chore: configurate db.

--- GanLink/Program.cs
+++ GanLink/Program.cs
@@ -1,23 +1,140 @@
+using GanLink.IAM.Application.Internal.CommandServices;
+using GanLink.IAM.Application.Internal.QueryServices;
+using GanLink.IAM.Domain.Repositories;
+using GanLink.IAM.Domain.Services;
+using GanLink.IAM.Infraestructure.Persistence.EF.Repositories;
+using GanLink.IAM.Infraestructure.Pipeline.Middleware.Extensions;
+using GanLink.Shared.Domain.Repositories;
+using GanLink.Shared.Infrastructure.Persistence.EFC.Configuration;
+using GanLink.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+//Adding Swagger as a Service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "GanLink.API",
+            Version = "v1",
+            Description = "ACME.GanLink.API",
+            TermsOfService = new Uri("https://ganlink.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "GanLink",
+                Email = "GanLink@gmail.com"
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Apache 2.0",
+                Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+            }
+        });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            []
+        }
+    });
+    options.EnableAnnotations();
+});
+
+//Add Controllers for manage our classes
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connection))
+{
+    throw new Exception("Database connection string not set");
+}
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString == null) throw new InvalidOperationException("Connection string not found");
+
+//Add our DbContext connection to Dependency Injector
+builder.Services.AddDbContext<GanLinkDBContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error);
+});
+
+// Shared
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<TimestampAudit>();
+
+//IAM
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserCommandService, UserCommandService>();
+builder.Services.AddScoped<IUserQueryService, UserQueryService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//Add scope for our DbContext
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var services = scope.ServiceProvider.GetRequiredService<GanLinkDBContext>();
+    services.Database.EnsureCreated();
 }
 
+app.UseCors("AllowAll");
+
+// Add Swagger for use on Development
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
+app.UseRequestAuthorizationMiddleware();
+//Mapping Controllers EndPoints
+app.MapControllers();
+
+app.Run();
 
 
 app.Run();
